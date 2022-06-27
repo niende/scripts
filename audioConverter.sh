06eed5300c205532d29a57d2cc00d74595e9f818
Nico Enderlein <nienderlein@gmail.com>
2022-06-27 18:16:22 +0200

    switched to lowercase variable names

--- audioConverter.sh
+++ audioConverter.sh
@@ -1,53 +1,52 @@
 #!/bin/bash
 
-OUTPUT_SUFFIX="_ENC"
-CODECS=("AC3" "DTS" "WAV" "FLAC")
-BITRATES_AC3=("96" "128" "192" "224" "256" "384" "448" "640")
-BITRATES_DTS=("768" "1510")
-BITRATES_WAV=("-")
-BITRATES_FLAC=("-")
-DRC_SCALES=("Off" "Partial" "Full")
-BITDEPTHS=("16" "24" "32")
-SAMPLERATES=("44100" "48000" "96000")
+output_suffix="_ENC"
+codecs=("AC3" "DTS" "FLAC" "WAV")
+bitrates_ac3=("96" "128" "192" "224" "256" "384" "448" "640")
+bitrates_dts=("768" "1510")
+bitrates_flac=("-")
+bitrates_wav=("-")
+drc_scales=("Off" "Partial" "Full")
+bitdepths=("16" "24" "32")
+samplerates=("44100" "48000" "96000")
 
-CODEC_INDEX=0
-BITRATE_INDEX=3
-DRC_INDEX=0
-BITDEPTH_INDEX=0
-SAMPLERATE_INDEX=1
+codec_index=0
+bitrate_index=3
+drc_index=0
+bitdepth_index=0
+samplerate_index=1
 
 GetCurrentBitrateArray () {
-    local BITRATE_ARRAY_NAME=BITRATES_$CURRENT_CODEC[@]
-    local CURRENT_BITRATE_ARRAY=${!BITRATE_ARRAY_NAME}
-    echo "${CURRENT_BITRATE_ARRAY[@]}"
+    local bitrate_array_name=bitrates_${current_codec,,}[@]
+    local current_bitrate_array=${!bitrate_array_name}
+    echo "${current_bitrate_array[@]}"
 }
 
 UpdateCurrentCodec () {
-    CURRENT_CODEC=${CODECS[$CODEC_INDEX]}
+    current_codec=${codecs[$codec_index]}
 }
 UpdateCurrentBitrate () {
-    BITRATE_ARRAY_NAME=BITRATES_$CURRENT_CODEC[$BITRATE_INDEX]
-    CURRENT_BITRATE=${!BITRATE_ARRAY_NAME}
-    # return $CURRENT_BITRATE
+    local bitrates=($(GetCurrentBitrateArray))
+    current_bitrate="${bitrates[$bitrate_index]}"
 }
 UpdateCurrentDrc () {
-    CURRENT_DRC=${DRC_SCALES[$DRC_INDEX]}
+    current_drc=${drc_scales[$drc_index]}
 }
 UpdateCurrentBitdepth () {
-    CURRENT_BITDEPTH=${BITDEPTHS[$BITDEPTH_INDEX]}
+    current_bitdepth=${bitdepths[$bitdepth_index]}
 }
 UpdateCurrentSamplerate () {
-    CURRENT_SAMPLERATE=${SAMPLERATES[$SAMPLERATE_INDEX]}
+    current_samplerate=${samplerates[$samplerate_index]}
 }
 
 CycleCodec () {
-    if [ $CODEC_INDEX -lt $((${#CODECS[@]} - 1)) ]; then
-        CODEC_INDEX=$((CODEC_INDEX + 1))
+    if [ $codec_index -lt $((${#codecs[@]} - 1)) ]; then
+        codec_index=$((codec_index + 1))
     else
-        CODEC_INDEX=0
+        codec_index=0
     fi
     # reset bitrate index to avoid index out of bounds...
-    BITRATE_INDEX=0
+    bitrate_index=0
 
     UpdateCurrentCodec
     UpdateCurrentBitrate
@@ -55,40 +54,39 @@ CycleCodec () {
     UpdateOutputFiles
 }
 CycleBitrate () {
-    local BITRATES=($(GetCurrentBitrateArray))
-    # GetCurrentBitrateArray
-    # local BITRATES=$?
-    if [ $BITRATE_INDEX -lt $((${#BITRATES[@]} - 1)) ]; then
-        BITRATE_INDEX=$((BITRATE_INDEX + 1))
+    local bitrates=($(GetCurrentBitrateArray))
+
+    if [ $bitrate_index -lt $((${#bitrates[@]} - 1)) ]; then
+        bitrate_index=$((bitrate_index + 1))
     else
-        BITRATE_INDEX=0
+        bitrate_index=0
     fi
 
     UpdateCurrentBitrate
 }
 CycleDrc () {
-    if [ $DRC_INDEX -lt $((${#DRC_SCALES[@]} - 1)) ]; then
-        DRC_INDEX=$((DRC_INDEX + 1))
+    if [ $drc_index -lt $((${#drc_scales[@]} - 1)) ]; then
+        drc_index=$((drc_index + 1))
     else
-        DRC_INDEX=0
+        drc_index=0
     fi
 
     UpdateCurrentDrc
 }
 CycleBitdepth () {
-    if [ $BITDEPTH_INDEX -lt $((${#BITDEPTHS[@]} - 1)) ]; then
-        BITDEPTH_INDEX=$((BITDEPTH_INDEX + 1))
+    if [ $bitdepth_index -lt $((${#bitdepths[@]} - 1)) ]; then
+        bitdepth_index=$((bitdepth_index + 1))
     else
-        BITDEPTH_INDEX=0
+        bitdepth_index=0
     fi
 
     UpdateCurrentBitdepth
 }
 CycleSamplerate () {
-    if [ $SAMPLERATE_INDEX -lt $((${#SAMPLERATES[@]} - 1)) ]; then
-        SAMPLERATE_INDEX=$((SAMPLERATE_INDEX + 1))
+    if [ $samplerate_index -lt $((${#samplerates[@]} - 1)) ]; then
+        samplerate_index=$((samplerate_index + 1))
     else
-        SAMPLERATE_INDEX=0
+        samplerate_index=0
     fi
 
     UpdateCurrentSamplerate
@@ -96,12 +94,12 @@ CycleSamplerate () {
 
 ChangeOutputFileSuffix () {
     echo ""
-    local NEW_SUFFIX=""
-    while [ -z "$NEW_SUFFIX" ] || [[ "$NEW_SUFFIX" = *" "* ]]; do
-        read -p "Enter new output file suffix: " NEW_SUFFIX
+    local new_suffix=""
+    while [ -z "$new_suffix" ] || [[ "$new_suffix" = *" "* ]]; do
+        read -p "Enter new output file suffix: " new_suffix
     done
 
-    OUTPUT_SUFFIX=$NEW_SUFFIX
+    output_suffix=$new_suffix
     UpdateOutputFiles
 }
 
@@ -114,36 +112,15 @@ function CountArrayItems {
     done
     echo $array_length
 }
-UpdateBitrateArrayLength () {
-    # get bitrates array for counting its items
-    BITRATE_ARRAY_FULL_NAME=BITRATES_$CURRENT_CODEC[@]
-    BITRATE_ARRAY_FULL=${!BITRATE_ARRAY_FULL_NAME}
-    # call function to count array items
-    CountArrayItems "${BITRATE_ARRAY_FULL}"
-    # get return value of function
-    BITRATE_ARRAY_LENGTH=$?
-}
 
-# SETTING $CURRENT_CODEC HERE
-UpdateCurrentCodec $CODEC_INDEX
-# SETTING CURRENT_BITRATE HERE
-UpdateCurrentBitrate $CURRENT_CODEC $BITRATE_INDEX
-# SETTING $CURRENT_DRC HERE
-UpdateCurrentDrc $DRC_INDEX
-# SETTING CURRENT_BITDEPTH HERE
-UpdateCurrentBitdepth $CURRENT_CODEC $BITRATE_INDEX
-# SETTING CURRENT_SAMPLERATE HERE
-# UpdateCurrentSamplerate $CURRENT_CODEC $BITRATE_INDEX
-
-# SETTING CODEC_ARRAY_LENGTH HERE
-CODEC_ARRAY_LENGTH=${#CODECS}
-# SETTING BITRATE_ARRAY_LENGTH HERE
-UpdateBitrateArrayLength
-# SETTING DRC_ARRAY_LENGTH HERE
-# DRC_ARRAY_LENGTH=${#DRC_SCALES}
+UpdateCurrentCodec
+UpdateCurrentBitrate
+UpdateCurrentDrc
+UpdateCurrentBitdepth
+UpdateCurrentSamplerate
 
 PropmtForInputFiles () {
-    read -p 'Please enter input file(s) comma separated: ' INPUT_FILES
+    read -p 'Please enter input file(s) separated by spaces: ' input_files
     # while [ ! -f $INPUT_FILE ]
     # do
     #     echo "File \"${INPUT_FILE}\" does not exist!"
@@ -158,19 +135,19 @@ PropmtForInputFiles () {
 
 UpdateOutputFiles () {
     # clear array before setting new output files
-    OUTPUT_FILES=()
-    for file in "${INPUT_FILES[@]}"; do
+    output_files=()
+    for file in "${input_files[@]}"; do
         # get file extension and set suffix for output files
         local extension=${file##*.}
         local extension_pos="$((${#file} - (${#extension} + 1)))"
 
         local file_name="${file:0:${extension_pos}}"
         #file_suffix="_ENC"
-        local file_extension=".${CURRENT_CODEC,,}"
+        local file_extension=".${current_codec,,}"
         # file_extension="${file:${extension_pos}}"
 
         # build output file names with suffix
-        OUTPUT_FILES+=("$file_name$OUTPUT_SUFFIX$file_extension")
+        output_files+=("$file_name$output_suffix$file_extension")
     done
 }
 
@@ -178,8 +155,8 @@ RequestFlacInstallation () {
     if ! command -v flac &> /dev/null; then
         echo "Flac package is not installed on your system, install it now?"
         echo "(y)es | (n)o"
-        read -n1 -p ': ' INSTALL_FLAC
-        if [ $INSTALL_FLAC = "y" ]; then
+        read -n1 -p ': ' install_flac
+        if [ $install_flac = "y" ]; then
             echo ""
             echo -n "Updating packages..."
             $(sudo apt update -qq 2>/dev/null >/dev/null)
@@ -194,23 +171,23 @@ RequestFlacInstallation () {
 
 GetInputFileInfo () {
     if [ -f $1 ]; then
-        INPUT_FILE_INFO=$(mediainfo --Inform=file://mediainfo_template $1)
+        input_file_info=$(mediainfo --Inform=file://mediainfo_template $1)
     else
-        INPUT_FILE_INFO='Invalid audio file, please choose another one!'
+        input_file_info='Invalid audio file, please choose another one!'
     fi
 }
 
 StartConversion () {
     # "${!INPUT_FILES[@]}" get array keys | "${INPUT_FILES[@]}" gets array values
-    for I in "${!INPUT_FILES[@]}"; do
-        drc_scale_str="-drc_scale $DRC_INDEX"
-        input_str="${INPUT_FILES[$I]}"
-        codec_str="-c:a ${CURRENT_CODEC,,}"
-        bitrate_str="-b:a ${CURRENT_BITRATE}k"
+    for i in "${!input_files[@]}"; do
+        drc_scale_str="-drc_scale $drc_index"
+        input_str="${input_files[$i]}"
+        codec_str="-c:a ${current_codec,,}"
+        bitrate_str="-b:a ${current_bitrate}k"
         hide_banner="-hide_banner"
-        output_str="${OUTPUT_FILES[$I]}"
+        output_str="${output_files[$i]}"
 
-        case "$CURRENT_CODEC" in
+        case "$current_codec" in
             "AC3")
                 echo "ffmpeg ${drc_scale_str} -i ${input_str} ${codec_str} ${bitrate_str} ${hide_banner} ${output_str}"
                 convert_command=(ffmpeg ${drc_scale_str} -i "${input_str}" ${codec_str} ${bitrate_str} ${hide_banner} "${output_str}")
@@ -255,25 +232,25 @@ StartConversion () {
 }
 
 # handle Drag'n'Drop here
-DND_COUNT=${#@}
-INPUT_FILES=()
+dnd_count=${#@}
+input_files=()
 files_no_ext=()
-OUTPUT_FILES=()
-if [ $DND_COUNT -gt 0 ]; then
-    FILES_STRING=""
-    for FILE in "$@"; do
-        INPUT_FILES+=("$FILE")
-        FILES_STRING=$FILES_STRING$FILE$'\n        '
+output_files=()
+if [ $dnd_count -gt 0 ]; then
+    files_string=""
+    for file in "$@"; do
+        input_files+=("$file")
+        files_string=$files_string$file$'\n        '
     done
 
     # generate output file names with current codec extension
     UpdateOutputFiles
 
-    if [ $DND_COUNT -gt 1 ]; then
+    if [ $dnd_count -gt 1 ]; then
         cat << EOF
 
-    Process $DND_COUNT file(s) in batch or dialog?
-        ${FILES_STRING}
+    Process $dnd_count file(s) in batch or dialog?
+        ${files_string}
     0: batch  |  1: dialog
 EOF
 
@@ -295,25 +272,25 @@ do
     FFMPEG Audio Converter (by qwk)
 ============================================================
 Audio info:
-${INPUT_FILE_INFO}
+${input_file_info}
 ------------------------------------------------------------
     Please enter your choice:
 
-    Input file(s)   (1): ${INPUT_FILES[@]/%/$nl                        }
-    Output file(s)  (2): ${OUTPUT_FILES[@]/%/$nl                        }
-    Output file suffix (o): ${OUTPUT_SUFFIX}$nl
-    Codec           (3): ${CURRENT_CODEC}
-    Bitrate (Kbps)  (4): ${CURRENT_BITRATE}
-    DRC             (5): ${CURRENT_DRC}
-    Bitdepth (bits) (6): ${CURRENT_BITDEPTH}
-    Samplerate (Hz) (7): ${CURRENT_SAMPLERATE}
+    Input file(s)   (1): ${input_files[@]/%/$nl                        }
+    Output file(s)  (2): ${output_files[@]/%/$nl                        }
+    Output file suffix (o): ${output_suffix}$nl
+    Codec           (3): ${current_codec}
+    Bitrate (Kbps)  (4): ${current_bitrate}
+    DRC             (5): ${current_drc}
+    Bitdepth (bits) (6): ${current_bitdepth}
+    Samplerate (Hz) (7): ${current_samplerate}
 
                     (s)tart
                     (q)uit
 ------------------------------------------------------------
 EOF
-    read -n1 -s -p '    : ' CHOICE
-    case "$CHOICE" in
+    read -n1 -s -p '    : ' choice
+    case "$choice" in
         "1")
             # PropmtForInputFile
             ;;
